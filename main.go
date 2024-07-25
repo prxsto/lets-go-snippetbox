791f8e4b1c88609ec8d54372d37736794347954c
prxsto <15711032+prxsto@users.noreply.github.com>
2024-07-25 13:12:27 -0700

    feat: init

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// print a log message when server starts
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err) // any errors returned are always non-nil
+}
